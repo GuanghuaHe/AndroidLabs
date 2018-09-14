40465a26efe3f1b9cc168a4d9513c9e9bc5faffd
GuanghuaHe <37890238+GuanghuaHe@users.noreply.github.com>
2018-09-14 15:43:15 -0400

    Added 3 activies for Lab 2

--- app/src/main/java/com/example/guanghuahe/androidlabs/StartActivity.java
+++ app/src/main/java/com/example/guanghuahe/androidlabs/StartActivity.java
@@ -1,13 +1,24 @@
 package com.example.guanghuahe.androidlabs;
 
+
 import android.app.Activity;
+
 import android.os.Bundle;
+
+
 
 public class StartActivity extends Activity {
 
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_start);
+
     }
+
 }
